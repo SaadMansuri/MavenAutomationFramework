f47229c626541c67f3ddf7eefce8240df1bf174d
unknown <chandrani.bhagat@cuelogic.co.in>
2014-04-15 19:34:19 +0530

    function names of class AddListing modified

--- Agorafy/test/agorafy/test/testcases/AddListing.java
+++ Agorafy/test/agorafy/test/testcases/AddListing.java
@@ -65,22 +65,20 @@ public class AddListing {
     @Test
     public void testAddListing() throws InterruptedException{
         
-        
-        //this.Login();
-        this.test_case1();
-        this.test_case2();
-        this.test_case3();
+        this.test_addListingTypePopup();
+        this.test_listingSaleTypePopup();
+        this.test_cancelAndBackOnPopup();
         Thread.sleep(4000);
-        this.test_case4();
-        this.test_case5();
-        this.test_case9();
-        this.test_case10();
-        this.test_case11();
-        test_case22();
+        this.test_urlChangeToManageListingUrl();
+        this.test_saveEmptyListing();
+        this.test_emptyListingStatusNotPublished();
+        this.test_emptyNewListingIsOnMarket();
+        this.test_verifyButtonDisabledForIncompleteListing();
+        test_mediaDecriptionPopup();
     }
     
    
-    public void test_case1() //creating new listing, check for pop-up
+    public void test_addListingTypePopup() //creating new listing, check for pop-up
     {
         propertyPageUrl = driver.getCurrentUrl();
         driver.findElement(By.id(VariableDeclarations.ADMIN_ID_ADDLISTING_BUTTON)).click();         //test case1 
@@ -88,7 +86,7 @@ public class AddListing {
             System.out.println("ERROR: dialog box for selecting type of listing not seen");  
         
     }
-    public void test_case2()    //creating new listing, check for pop-up
+    public void test_listingSaleTypePopup()    //creating new listing, check for pop-up
     {
         driver.findElement(By.id(VariableDeclarations.ADMIN_ADDLISTING_ALERT1_RESIDENTIAL_BUTTON_ID)).click();
         if(!(this.isElementPresent(By.xpath(VariableDeclarations.ADMIN_ADDLISTING_ALERT1))))            //test case 2
@@ -97,7 +95,7 @@ public class AddListing {
             System.out.println("ERROR: dialog box for selecting type of listing not seen");  
         
     }
-    public void test_case3()    //creating new listing, check for cancel and back buttons
+    public void test_cancelAndBackOnPopup()    //creating new listing, check for cancel and back buttons
     {
         if(!(this.isElementPresent(By.xpath(VariableDeclarations.ADMIN_ADDLISTING_ALERT12_BACK_XPATH)))) //test 3
             System.out.println("ERROR: back button on pop up not seen");  
@@ -106,7 +104,7 @@ public class AddListing {
         
     }
     
-    public void test_case4()    //check if manage listing page is seen after add listing operation
+    public void test_urlChangeToManageListingUrl()    //check if manage listing page is seen after add listing operation
     {
        driver.findElement(By.id(VariableDeclarations.ADMIN_ADDLISTING_ALERT2_SALE_BUTTON_ID)).click();
        if(!driver.getCurrentUrl().contains("manage/admin/listing/add/residential"))
@@ -114,7 +112,7 @@ public class AddListing {
        
     }
     
-    public void test_case5()    //after save listing should get added, appearance of buttons changes
+    public void test_saveEmptyListing()    //after save, listing should get added, appearance of buttons changes
     {
         String DisplayName = driver.findElement(By.id(VariableDeclarations.ADMIN_ID_DISPLAYNAME_LISTING)).getText();
         driver.findElement(By.id(VariableDeclarations.ADMIN_ID_NEWLISTING_SAVE_BUTTON)).click();
@@ -122,32 +120,28 @@ public class AddListing {
             System.out.println("ERROR: Listing did not get saved");        
     }
     
-    public void test_case9()    //incomplete listing should dhow Not Published
+    public void test_emptyListingStatusNotPublished()    //incomplete listing should show Not Published
     {
-       /* String notPublished = driver.findElement(By.id("published")).getAttribute(disabled);
-        System.out.println(notPublished);
-        if(!notPublished.contentEquals("disabled"))
-            System.out.println("ERROR: status id not 'Not Published'");*/
-        
+               
         if(!(driver.findElement(By.id("formContainer")).getText().contains("Not Published")))
             System.out.println("ERROR: status id is not 'Not Published'");
         
     }
     
-    public void test_case10()   //check if new listing is "On market" by default
+    public void test_emptyNewListingIsOnMarket()   //check if new listing is "On market" by default
     {
         if(!(driver.findElement(By.id("formContainer")).getText().contains("On the Market")))
             System.out.println("ERROR: status of listing is not 'On the Market'");
     }
     
-    public void test_case11()
+    public void test_verifyButtonDisabledForIncompleteListing() //check if verify button is disabled for incomplete listing
     {
        if(!driver.findElement(By.id("verifyToggle")).getAttribute("aria-disabled").contentEquals("true"))
           //aria-disabled is TRUE if verify button is disabled
           System.out.println("ERROR: verify button is not disabled");
     }
     
-    public void test_case22()
+    public void test_mediaDecriptionPopup()
     {
         driver.get("http://preview.agorafy.com/manage/admin/listing/470");
         driver.findElement(By.className("dropzoneThumbnail ui-draggable")).click();
