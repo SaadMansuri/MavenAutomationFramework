544a017db6b41906d41dec42329f5a6bb77c13da
unknown <chandrani.bhagat@cuelogic.co.in>
2014-04-29 15:27:55 +0530

    added new package for listings and related test scripts

--- Agorafy/test/agorafy/test/testcases/listings/Listings.java
+++ Agorafy/test/agorafy/test/testcases/listings/Listings.java
@@ -6,7 +6,7 @@
  * 
  */
 
-package agorafy.test.testcases;
+package agorafy.test.testcases.listings;
 
 
 import agorafy.test.utils.*;
@@ -21,6 +21,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import agorafy.test.operations.Login;
 import agorafy.test.operations.Logout;
+import agorafy.test.testcases.listings.*;
 import org.openqa.selenium.support.ui.Select;
 
 
@@ -72,7 +73,8 @@ public class Listings {
         this.test_emptyListingStatusNotPublished();
         this.test_emptyNewListingIsOnMarket();
         this.test_verifyButtonDisabledForIncompleteListing();
-        test_addResidentialListing();
+        ResidentialListings.test_addResidentialListing(driver);
+        ResidentialListings.test_editResidentialListingLeaseToSale(driver);
        // test_mediaDecriptionPopup();
         
     }
@@ -141,58 +143,7 @@ public class Listings {
           System.out.println("ERROR: verify button is not disabled");
     }
       
-    public void test_addResidentialListing() throws InterruptedException
-    {
-        
-        driver.findElement(By.id(VariableDeclarations.ADMIN_ID_DISPLAYNAME_LISTING)).sendKeys("Residential R - Sale");
-        
-        Select dropdownForSpaceType = new Select(driver.findElement(By.id("spaceType")));   //wrapping the web element in select object
-        dropdownForSpaceType.selectByVisibleText("Unit");      
-        
-        driver.findElement(By.id(VariableDeclarations.ADMIN_RESIDENTIAL_LISTING_SPACE_ID)).clear();
-        driver.findElement(By.id(VariableDeclarations.ADMIN_RESIDENTIAL_LISTING_SPACE_ID)).sendKeys("Apt #1");
-        Thread.sleep(3000);
-        driver.findElement(By.id(VariableDeclarations.ADMIN_RESIDENTIAL_NUMBEROFBEDS_ID)).clear();
-        driver.findElement(By.id(VariableDeclarations.ADMIN_RESIDENTIAL_NUMBEROFBEDS_ID)).sendKeys("2");
-        Thread.sleep(3000);
-        driver.findElement(By.id(VariableDeclarations.ADMIN_RESIDENTIAL_NUMBEROFBATHS_ID)).clear();
-        driver.findElement(By.id(VariableDeclarations.ADMIN_RESIDENTIAL_NUMBEROFBATHS_ID)).sendKeys("2");
-        Thread.sleep(3000);
-        driver.findElement(By.name("form[unitPrice]")).clear();
-        driver.findElement(By.name("form[unitPrice]")).sendKeys("109,090");
-        
-        driver.findElement(By.name("form[unitSquareFootage][]")).clear();
-        driver.findElement(By.name("form[unitSquareFootage][]")).sendKeys("800");
-        
-        driver.findElement(By.name("form[maintenance]")).clear();
-        driver.findElement(By.name("form[maintenance]")).sendKeys("250");
-        Thread.sleep(3000);
-        driver.findElement(By.id("addContactLink")).click();
-        Thread.sleep(3000);
-        System.out.println(isElementPresent.isElementPresent(By.id("contactNewContainer"), driver));
-        System.out.println(driver.findElement(By.id("contactNewContainer")));
-        
-        //for autocomplete this is how it is to be handled
-        WebElement popupContactSearch = driver.switchTo().activeElement().findElement(By.id("contactSearch"));
-        popupContactSearch.sendKeys("Pranoop R", Keys.PAUSE);
-        Thread.sleep(5000);
-        popupContactSearch.sendKeys( Keys.DOWN);
-        popupContactSearch.sendKeys( Keys.TAB);
-        Thread.sleep(5000);
-        driver.findElement(By.id("addContactButton")).click();
-        Thread.sleep(5000);
-        
-        if(driver.findElement(By.id("dropzone")).getAttribute("class").equalsIgnoreCase("redborder"))
-        {
-            System.out.println("Message: Incomplete residential form");
-            if(!driver.findElement(By.id("publishedDiv")).getText().equalsIgnoreCase("not published"))
-                System.out.println("ERROR: Form is incomplete but Not Published text is missing");
-                
-        }
-                              
-        
-        driver.findElement(By.id("save")).click();
-    }
+   
     
     public void test_mediaDecriptionPopup()
     {
