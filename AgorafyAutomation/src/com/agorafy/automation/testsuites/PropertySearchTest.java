7fa44f2136860a99088e9b72e586e7dd93cf4408
ajinkyab-cuelogic <ajinkya.bhagat@cuelogic.co.in>
2014-11-28 17:09:22 +0530

    Added new file PropertySearchTest in testsuit

---
+++ AgorafyAutomation/src/com/agorafy/automation/testsuites/PropertySearchTest.java
@@ -0,0 +1,31 @@
+package com.agorafy.automation.testsuites;
+
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import com.agorafy.automation.automationframework.AutomationFramework;
+import com.agorafy.automation.testcases.PropertySearchAction;
+
+public class PropertySearchTest 
+{
+    @BeforeSuite
+    public void Init()
+     {
+        String globalConfigureationFileWithPath = "src/com/agorafy/automation/configuration/config.properties";
+        AutomationFramework.initWithGlobalConfiguration(globalConfigureationFileWithPath);
+     }
+
+    @Test
+    public void propertySearchActionTestCases() throws Exception
+    {
+        try
+        {
+            new PropertySearchAction().Execute();
+        }
+        catch (Exception e) 
+        {
+        	System.out.println(e.getMessage());
+            throw (e);
+        }
+    }
+}
