b93b7e4a251127bfdbfc4d46b2c585441a949510
ajinkyab-cuelogic <ajinkya.bhagat@cuelogic.co.in>
2014-11-28 16:57:09 +0530

    Added changes in Homepage related to Search bar

--- AgorafyAutomation/src/com/agorafy/automation/pageobjects/Homepage.java
+++ AgorafyAutomation/src/com/agorafy/automation/pageobjects/Homepage.java
@@ -46,6 +46,126 @@ public class Homepage extends Page
         return element;
     }
 
+    public WebElement txtbx_SearchTerm() throws Exception
+    {
+        try
+        {
+            element= driver.findElement(By.id("searchInput"));
+            AutomationLog.info("SearchTerm text box found ");
+        }
+        catch(Exception e)
+        {
+            AutomationLog.error("Could not found SearchTerm text box ");
+            throw(e);
+        }
+        return element;
+    }
+
+    public WebElement dropdown_SelectListingCategoryIcon() throws Exception 
+    {
+        try
+        {
+            element=driver.findElement(By.xpath(".//*[@id='listingCategorySelect']/div/span"));
+            AutomationLog.info("Select Listing Catrgory Icon found ");
+        }
+        catch(Exception e)
+        {
+            AutomationLog.error("Could not found Select Listing Catrgory Icon ");
+            throw(e);
+        }
+        return element;
+    }
+
+    public void clickOnSelectListingCategoryIcon() throws Exception
+    {
+        try
+        {
+            dropdown_SelectListingCategoryIcon().click();
+            AutomationLog.info("Successfully clicked on SelectListingCategoryIcon");
+        }
+        catch(Exception e)
+        {
+            AutomationLog.error("Could not click on SelectListingCategoryIcon");
+            throw(e);
+        }
+    }
+    public WebElement dropdownOption_Residential() throws Exception
+    {
+        try
+        {
+            element= driver.findElement(By.xpath(".//*[@id='listingCategorySelect']/ul/li[2]/a"));
+            AutomationLog.info("Found residential category in dropdown");
+        }
+        catch(Exception e)
+        {
+            AutomationLog.error("Could not found Residential category in dropdown");
+            throw(e);
+        }
+        return element;
+    }
+
+    public void selectResidentialListingCategory() throws Exception
+    {
+        try
+        {
+            dropdownOption_Residential().click();
+            AutomationLog.info("Successfully selected residential category option ");
+        }
+        catch(Exception e)
+        {
+            AutomationLog.error("Could not select Residential category option");
+            throw(e);
+        }
+    }
+    public WebElement btn_Search() throws Exception 
+    {
+        try
+        {
+            element=driver.findElement(By.id("searchFormButton"));
+            AutomationLog.info("Search button found");
+        }
+        catch(Exception e)
+        {
+            AutomationLog.error("Could not found Search button");
+            throw(e);
+        }
+        return element;
+    }
+
+    public void clickOnSearchButtton() throws Exception
+    {
+        try
+        {
+            btn_Search().click();
+            AutomationLog.info("Clicked on search button");
+        }
+        catch(Exception e)
+        {
+            AutomationLog.error("Could not clicked on Search button");
+            throw(e);
+        }
+    }
+
+    public PropertySearch populateSearchTermTextBox() throws Exception
+    {
+        PropertySearch propertysearch=null;
+        try
+        {
+            clickOnSelectListingCategoryIcon();
+            selectResidentialListingCategory();
+            txtbx_SearchTerm().sendKeys("Rentals in 10010");
+            clickOnSearchButtton();
+            propertysearch=new PropertySearch(driver);
+            AutomationLog.info("Search is performed successfully");
+        }
+        catch(Exception e)
+        {
+            AutomationLog.error("Could not performed Search");
+            throw(e);
+        }
+        return propertysearch;
+    }
+
     public HeaderLoginForm openHeaderLoginForm() throws Exception
     {
         HeaderLoginForm element = null;
