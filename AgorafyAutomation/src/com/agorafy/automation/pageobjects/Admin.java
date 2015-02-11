c1d5d7cda87578a8285e724fe64c3013073e7c1b
satishp-cuelogic <satish.patil@cuelogic.co.in>
2015-02-11 19:27:27 +0530

    Added following page objects- Admin, Reports

---
+++ AgorafyAutomation/src/com/agorafy/automation/pageobjects/Admin.java
@@ -0,0 +1,12 @@
+package com.agorafy.automation.pageobjects;
+
+import org.openqa.selenium.WebDriver;
+
+public class Admin extends Page 
+{
+
+    public Admin(WebDriver driver) 
+    {
+        super(driver);
+    }
+}
---
+++ AgorafyAutomation/src/com/agorafy/automation/pageobjects/Reports.java
@@ -0,0 +1,13 @@
+package com.agorafy.automation.pageobjects;
+
+import org.openqa.selenium.WebDriver;
+
+public class Reports extends Page 
+{
+
+    public Reports(WebDriver driver) 
+    {
+        super(driver);
+
+    }
+}
