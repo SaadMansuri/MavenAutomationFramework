77d91c9bffc33f919155522948bd9f83bca18253
satishp-cuelogic <satish.patil@cuelogic.co.in>
2015-02-11 19:30:13 +0530

    Added following Action classes- SubscriptionsAction, SubscriptionsTest

---
+++ AgorafyAutomation/src/com/agorafy/automation/testsuites/SubscriptionsTest.java
@@ -0,0 +1,32 @@
+package com.agorafy.automation.testsuites;
+
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import com.agorafy.automation.automationframework.AutomationFramework;
+import com.agorafy.automation.testcases.SubscriptionsAction;
+/*Test Subscription Pages*/
+
+
+public class SubscriptionsTest 
+{
+    @BeforeSuite
+    public void Init() throws Exception
+    {
+        String globalConfigureationFileWithPath = "src/com/agorafy/automation/configuration/config.properties";
+        AutomationFramework.initWithGlobalConfiguration(globalConfigureationFileWithPath);
+    }
+
+    @Test
+    public void testSubscriptionsLink() throws Exception
+    {
+        try
+        {
+            new SubscriptionsAction().Execute();
+        }
+        catch(Exception e)
+        {
+            throw(e);
+        }
+    }
+}
