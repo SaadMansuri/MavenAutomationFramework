b4baf0069328bc13b0585aa9712a30857f5883dc
chandrani <chandrani.bhagat@cuelogic.co.in>
2015-03-03 20:34:50 +0530

    Committed files related to upsell login popups throughout the site

---
+++ AgorafyAutomation/src/com/agorafy/automation/testcases/upsellpopups/ProfessionalProfileLoginUpsellAction.java
@@ -0,0 +1,81 @@
+package com.agorafy.automation.testcases.upsellpopups;
+
+import java.util.HashMap;
+
+import org.testng.Assert;
+
+import com.agorafy.automation.automationframework.AutomationLog;
+import com.agorafy.automation.automationframework.AutomationTestCaseVerification;
+import com.agorafy.automation.automationframework.Credentials;
+import com.agorafy.automation.pageobjects.Page;
+import com.agorafy.automation.pageobjects.upsellpopups.ListingDetailPage;
+import com.agorafy.automation.pageobjects.upsellpopups.LoginPopUp;
+import com.agorafy.automation.pageobjects.upsellpopups.ProfessionalProfilePage;
+
+public class ProfessionalProfileLoginUpsellAction extends AutomationTestCaseVerification 
+{
+    private ProfessionalProfilePage professionalProfilePage = null;
+    private LoginPopUp loginpopup = null;
+
+    public ProfessionalProfileLoginUpsellAction()
+    {
+        super();
+    }
+
+    public void setup()
+    {
+        professionalProfilePage = ProfessionalProfilePage.professionalProfilePage();
+        try
+        {
+            super.setup();
+            Page.driver.get("http://www.agorafy.com/profile/44386");
+            AutomationLog.info("Able to Navigate to Professional Page popup");
+            
+        }
+        catch (Exception e)
+        {
+            AutomationLog.error("Could not navigate to Professional Page");
+        }
+    }
+
+@Override
+     protected void verifyTestCases() throws Exception
+     {
+            Credentials validCredentials = userCredentials();
+            //HashMap<String, String> getvalidcrendial = testCaseData.get("validCredential");
+            verifysendEmailLinkLoginUpsell(professionalProfilePage,validCredentials);
+     }
+
+    public void verifysendEmailLinkLoginUpsell(ProfessionalProfilePage professionalPopUp, Credentials getvalidcrendial) throws Exception
+    {
+        try
+        {
+            loginpopup = professionalProfilePage.clickSendEmailbtnOnProfessionalProfile();
+            String Url = Page.driver.getCurrentUrl();
+            Thread.sleep(5000);
+            Assert.assertEquals(loginpopup.checkingLogInPopUp(), true, "Upsell Login popup Not found");
+            loginpopup.populateLoginPopUpData(getvalidcrendial.getEmail(), getvalidcrendial.getPassword());
+            professionalProfilePage = (ProfessionalProfilePage) loginpopup.clickLoginButtonOnUpsell();
+
+            //professionalPopUp.populateLoginPopUpDataForProfessionalPage(getvalidcrendial.get("username"),getvalidcrendial.get("password"));
+            Assert.assertEquals(professionalProfilePage.currentURL(),Url,"unsuccessfull login after entering valid credentials for Professional Page" );
+            AutomationLog.info("successfull login after entering valid credentials for Professional Page");
+        }
+        catch(Exception e)
+        {
+            AutomationLog.error("could not send credentials to login pop up of Professional Page");
+        }
+    }
+
+    @Override
+    protected String successMessage()
+    {
+        return "Funtionality of Send Email button on ProfessionalPage is Passed";
+    }
+
+    @Override
+    protected String failureMessage()
+    {
+        return "Funtionality of Send Email button on ProfessionalPage is Failed";
+    }
+}
--- AgorafyAutomation/src/com/agorafy/automation/testsuites/LoginAttemptsTest.java
+++ AgorafyAutomation/src/com/agorafy/automation/testsuites/LoginAttemptsTest.java
@@ -4,23 +4,25 @@ import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 import com.agorafy.automation.automationframework.AutomationFramework;
-import com.agorafy.automation.testcases.upsellpopups.ProfessionalProfileLoginUpsellAction;
+import com.agorafy.automation.testcases.LoginAttemptsAction;
 
-public class ProfessionalPageTest {
-
+public class LoginAttemptsTest
+{
     @BeforeSuite
-    public void Init()
+    public void Init() throws Exception
     {
+        // TODO: Move this to some TestNg XML configuration file, so that we can set config file path
+        // when running in headless mode.
         String globalConfigureationFileWithPath = "src/com/agorafy/automation/configuration/config.properties";
         AutomationFramework.initWithGlobalConfiguration(globalConfigureationFileWithPath);
     }
 
-@Test
-    public void testProfessionalProfilePageTestCase() throws Exception
+    @Test
+    public void testCaptchaAppearance() throws Exception
     {
         try
         {
-            new ProfessionalProfileLoginUpsellAction().Execute();
+            new LoginAttemptsAction().Execute();
         }
         catch (Exception e)
         {
