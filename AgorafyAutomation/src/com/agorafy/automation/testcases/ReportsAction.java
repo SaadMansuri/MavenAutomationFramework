9601ac9d14c1b87c5afff8f4c56e64aad95c8f4a
ajinkyab-cuelogic <ajinkya.bhagat@cuelogic.co.in>
2015-03-24 21:19:29 +0530

    Added test cases for print link on ReportsBox

--- AgorafyAutomation/src/com/agorafy/automation/testcases/ReportsAction.java
+++ AgorafyAutomation/src/com/agorafy/automation/testcases/ReportsAction.java
@@ -65,8 +65,6 @@ public class ReportsAction extends AutomationTestCaseVerification
     {
         verifySearchResultPageTestCases();
         verifyListingDetailPageTestCases();
-        verifyIfReportsCountIsSameOnBothWindows();
-        verifyIfAddingMoreThanFifteenListingShowsErrorDialog();
        
     }
 
@@ -82,6 +80,12 @@ public class ReportsAction extends AutomationTestCaseVerification
         verifyIfClickingOnClearLinkOnReportsBoxClearsReportsList();
         verifyIfClickedOnCloseIconForIndividualListingInReportsBox();
         
+        verifyIfReportsCountIsSameOnBothWindows();
+        verifyIfAddingMoreThanFifteenListingShowsErrorDialog();
+        verifyIfClickingCloseIconOnReportsListRemovesThatListing();
+        
+        verifyIfPrintLinkIsShownOnReportsBox();
+        verifyIfClickingPrintLinkOpensPopUp();
     }
 
     public void verifyListingDetailPageTestCases() throws Exception
@@ -299,12 +303,14 @@ public class ReportsAction extends AutomationTestCaseVerification
     public void verifyIfReportsCountIsSameOnBothWindows() throws Exception
     {
         boolean isLoggedIn = true;
-        searchresult.clickSearchResult();
+        listingdetail = searchresult.clickSearchResult();
         SwitchToNextWindow();
         listingdetail.clickOnAddToReportLink(isLoggedIn);
         int listingpagecount = Integer.parseInt(reports.getReportCount());
         Page.driver.close();
         Page.driver.switchTo().window(curHandle);
+        Page.driver.navigate().refresh();
+        WaitFor.sleepFor(2000);
         int searchpagecount = Integer.parseInt(reports.getReportCount());
         Assert.assertEquals(listingpagecount, searchpagecount, "Expected Reports Count is not increased on both windows ");
         header.clickOnProfileNameDropdownArrow();
@@ -349,7 +355,33 @@ public class ReportsAction extends AutomationTestCaseVerification
             }
         }
         Assert.assertTrue(searchresult.popup_ErrorDialog().isDisplayed(), "Expected Error dialog pop up is not shown");
+        searchresult.clickCloseIconOnErrorDialogPopUp();
         AutomationLog.info("Error Dialog is shown if more than fifteen listings tried to add ");
+    }
+
+    public void verifyIfClickingCloseIconOnReportsListRemovesThatListing() throws Exception
+    {
+        int index = 0;
+        header.clickOnProfileNameDropdownArrow();
+        reports = header.clickOnReportsLink();
+        WaitFor.sleepFor(2000);
+        reports.hoverOnFirstDeleteListingIcon();
+        reports.clickOnFirstDeleteListingIcon();
+        Assert.assertFalse(searchresult.icon_PinCushion(index).isDisplayed(), "Expected AddToReport icon for first listingd is not unFixed");
+        AutomationLog.info("On Removing First listing by clicking close icon in Reports List unfix AddToReport icon for the first Listing in Search Results");
+    }
+
+    public void verifyIfPrintLinkIsShownOnReportsBox() throws Exception
+    {
+        Assert.assertTrue(reports.link_Print().isDisplayed(), "Excpected pincushionl Link is not present");
+    }
+
+    public void verifyIfClickingPrintLinkOpensPopUp() throws Exception 
+    {
+        reports.clickOnPrintLink();
+        Assert.assertTrue(reports.popup_print().isDisplayed(), "Expected popup is not shown");
+        reports.clickOnCloseButtonOnPrintReportsPopUp();
+        AutomationLog.info("Clicking on print link shows popup");
     }
 
     @Override
