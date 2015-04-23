dc40c5cbc1255a3e47374b002645dedccb4a06a5
ajinkyab-cuelogic <ajinkya.bhagat@cuelogic.co.in>
2015-04-23 21:31:30 +0530

    Created class and added login method

---
+++ AgorafyAutomation/src/com/agorafy/automation/utilities/Login.java
@@ -0,0 +1,32 @@
+package com.agorafy.automation.utilities;
+
+
+import com.agorafy.automation.automationframework.AutomationLog;
+import com.agorafy.automation.automationframework.Credentials;
+import com.agorafy.automation.pageobjects.Header;
+import com.agorafy.automation.pageobjects.HeaderLoginForm;
+import com.agorafy.automation.pageobjects.Homepage;
+import com.agorafy.automation.automationframework.AutomationTestCase;
+
+public class Login 
+{
+
+    public static Homepage doSuccessfullLoginFromHeaderLoginForm() throws Exception 
+    {
+        Homepage homepage = null;
+        try
+        {
+            Header header = Header.header();
+            HeaderLoginForm headerLoginForm = header.openHeaderLoginForm();
+            AutomationLog.info("Log In link is clicked");
+            Credentials ValidCredentials = AutomationTestCase.userCredentials();
+            homepage = headerLoginForm.doSuccessfulLogin(ValidCredentials.getEmail(), ValidCredentials.getPassword());
+            AutomationLog.info("Login Successfull");
+        }
+        catch(Exception e)
+        {
+            AutomationLog.error("Login failed");
+        }
+        return homepage;
+    }
+}
